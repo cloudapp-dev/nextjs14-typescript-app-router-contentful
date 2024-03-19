8b94c2a25a604c5a802c7b803e0c6c5c02f8f426
Lireking <avantsystem@gmail.com>
2024-03-19 17:15:04 +0100

    DraftMode Component added to layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,13 @@
 import type { Metadata } from "next";
 import { Urbanist } from "next/font/google";
+import { draftMode } from "next/headers";
 import "./globals.css";
 import Header from "@/components/header/header.component";
 import Footer from "@/components/footer/footer.component";
 import { Providers } from "@/components/header/providers";
 import getAllNavitemsForHome from "@/components/header/navbar.menuitems.component";
 import getAllFooteritemsForHome from "@/components/footer/footer.menuitems.component";
+import ExitDraftModeLink from "@/components/header/draftmode/ExitDraftModeLink.component";
 
 const urbanist = Urbanist({ subsets: ["latin"], variable: "--font-urbanist" });
 
@@ -28,6 +30,11 @@ export default async function RootLayout({
         <main className={`${urbanist.variable} font-sans dark:bg-gray-900`}>
           <Providers>
             <Header menuItems={headerdata} />
+            {draftMode().isEnabled && (
+              <p className="bg-orange-200 py-4 px-[6vw]">
+                Draft mode is on! <ExitDraftModeLink className="underline" />
+              </p>
+            )}
             {children}
             <Footer footerItems={footerdata} />
           </Providers>
