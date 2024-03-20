08bc4cea8f0ce9ff53adbadb81165c80453eb83e
Lireking <avantsystem@gmail.com>
2024-03-20 07:35:03 +0100

    code change in next.config.js due to upgrade to Nextjs 14

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: { domains: ["images.ctfassets.net"] },
-  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.ctfassets.net",
+        pathname: "**",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
